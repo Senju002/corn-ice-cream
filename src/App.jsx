0e1e3cfb82841877d64390cf45a1ea850344aa0a
Senju002 <rajkaran.mei34@gmail.com>
2024-03-10 17:17:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+// import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
